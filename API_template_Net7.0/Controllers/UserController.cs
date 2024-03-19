5507da65340ee838f4027e82d6fabe610b021a74
elim <elim@acube.com.tw>
2024-03-19 11:06:02 +0800

    feat: 調整路徑

--- API_template_Net7.0/Controllers/UserController.cs
+++ API_template_Net7.0/Controllers/UserController.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Hosting;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace mm.Controllers
+namespace Controllers
 {
     [Route("api/user")]
     public class UserController : Controller
